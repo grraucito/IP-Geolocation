ccad063a6eafe272b38ad9ec7fca50500e862b78
Jorge Anaya <jorge.anaya@triplei.mx>
2022-02-10 12:46:32 -0600

    Agregar archivos de proyecto.

---
+++ IP Geolocation/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using IP_Geolocation.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IP_Geolocation
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private RootObject rootObject;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //rootObject = new RootObject();
+        }
+        
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            rootObject = await GeolocationProxy.GetGeolocation();
+            if (rootObject.status.Equals("success"))
+            {
+                AsNumber.Text = rootObject.@as;
+                city.Text = rootObject.city;
+                country.Text = rootObject.country;
+                countryCode.Text = rootObject.countryCode;
+                isp.Text = rootObject.isp;
+                lat.Text = rootObject.lat.ToString();
+                lon.Text = rootObject.lon.ToString();
+                org.Text = rootObject.org;
+                query.Text = rootObject.query;
+                region.Text = rootObject.region;
+                regionName.Text = rootObject.regionName;
+                timezone.Text = rootObject.timezone;
+                zip.Text = rootObject.zip;
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
---
+++ IP Geolocation/Models/GeolocationProxy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP_Geolocation.Models
+{
+    public class GeolocationProxy
+    {
+        public async static Task<RootObject> GetGeolocation()
+        {
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync("http://ip-api.com/json");
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+
+            return data;
+        }
+    }
+    
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public string @as { get; set; }
+        [DataMember]
+        public string city { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public string countryCode { get; set; }
+        [DataMember]
+        public string isp { get; set; }
+        [DataMember]
+        public double lat { get; set; }
+        [DataMember]
+        public double lon { get; set; }
+        [DataMember]
+        public string org { get; set; }
+        [DataMember]
+        public string query { get; set; }
+        [DataMember]
+        public string region { get; set; }
+        [DataMember]
+        public string regionName { get; set; }
+        [DataMember]
+        public string status { get; set; }
+        [DataMember]
+        public string timezone { get; set; }
+        [DataMember]
+        public string zip { get; set; }
+        [DataMember]
+        public string message { get; set; }
+    }
+}
